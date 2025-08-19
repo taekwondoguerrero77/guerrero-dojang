de3fb8e207c64f071df3a3040c08cb43c1358f2b
GuerreroDojang <taekwondoguerrero77@gmail.com>
2025-08-19 20:44:07 +0200

    latest changes in web

--- main.js
+++ main.js
@@ -26,6 +26,20 @@ navLinks.forEach(link => {
 });
 
 
+/* ==================== MARCAR ENLACE ACTIVO ==================== */
+const navLinksPage = document.querySelectorAll('.nav__link');
+const currentPage = window.location.pathname.split('/').pop();
+
+navLinksPage.forEach(link => {
+  const linkPage = link.getAttribute('href').split('/').pop();
+
+  // Si la URL del enlace coincide con la página actual, le añade la clase 'active-link'
+  if (linkPage === currentPage || (currentPage === '' && linkPage === 'index.html')) {
+    link.classList.add('active-link');
+  }
+});
+
+
 /* ==================== CAMBIAR COLOR DEL HEADER AL HACER SCROLL ==================== */
 function scrollHeader(){
     const header = document.querySelector('.header');
